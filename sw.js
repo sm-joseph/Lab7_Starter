5a20d68ad996eaae89529e8934c2cd745ccb9f38
Sanjit Joseph <78522615+sm-joseph@users.noreply.github.com>
2022-11-14 20:23:31 -0800

    Respond to fetch events with cache

--- sw.js
+++ sw.js
@@ -39,9 +39,18 @@ self.addEventListener('fetch', function (event) {
   //       fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
   /*******************************/
+
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  event.respondWith(caches.open(CACHE_NAME).then( async (cache) => {
+    return cache.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+        // B8. TODO - If the request is in the cache, return with the cached version.
+        //            Otherwise fetch the resource, add it to the cache, and return
+        //            network response.
+        cache.put(event.request, fetchedResponse.clone());
+        return fetchedResponse;
+      });
+    });
+  }));
 });
