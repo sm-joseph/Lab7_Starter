ddfe48f40ec6df3b51c11a915efa0f1b3659028e
Sanjit Joseph <78522615+sm-joseph@users.noreply.github.com>
2022-11-14 18:08:11 -0800

    Return promise for A4

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -81,6 +81,7 @@ async function getRecipes() {
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
   /**************************/
+  return new Promise((resolve, reject) => {
   // A4-A11 will all be *inside* the callback function we passed to the Promise
   // we're returning
   /**************************/
@@ -103,6 +104,7 @@ async function getRecipes() {
   //            resolve() method.
   // A10. TODO - Log any errors from catch using console.error
   // A11. TODO - Pass any errors to the Promise's reject() function
+  });
 }
 
 /**
